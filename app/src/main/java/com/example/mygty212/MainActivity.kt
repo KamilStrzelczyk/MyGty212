fdd15c4779fa0c526088f4911d3d5830ffca2da1
Kamil Strzelczyk <73589639+KamilStrzelczyk@users.noreply.github.com>
2022-03-29 15:58:26 +0200

    thirdtext

--- app/src/main/java/com/example/mygty212/MainActivity.kt
+++ app/src/main/java/com/example/mygty212/MainActivity.kt
@@ -9,5 +9,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         println("cmmit")
         println("secondcommit")
+        println("thirdcommits")
     }
 }
