1a50693c30a47448243bd8c6e5eda2b22d4194fd
Kamil Strzelczyk <73589639+KamilStrzelczyk@users.noreply.github.com>
2022-04-05 12:13:44 +0200

    THIS IS FIRST BRANCH

--- app/src/main/java/com/example/mygty212/MainActivity.kt
+++ app/src/main/java/com/example/mygty212/MainActivity.kt
@@ -7,6 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        println("THIS IS FIRTST BRANCH")
         println("cmmit")
         println("secondcommit")
         println("thirdcommits")
